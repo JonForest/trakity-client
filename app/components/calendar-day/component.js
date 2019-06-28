e2f6a49a964e08890a6d7fc53b469a542fe5f128
Jonathan Hollingsworth <jonforest@gmail.com>
2019-06-29 08:32:29 +1200

    Minor code tidy - squash this out

--- app/components/calendar-day/component.js
+++ app/components/calendar-day/component.js
@@ -13,7 +13,8 @@ function convertModelToEvent(taskModel) {
 export default class CalendarDayComponent extends Component {
   //As the next action, really need to think through this workflow
   // - How can we drag tasks onto the calendar?
-  // - Do we really want to render _all_ tasks in the calendar (might be okay tbh)
+  // - Do we really want to render _all_ tasks in the calendar (might be okay tbh
+  //    - especially as the default is to _not_ display on the calendar)
   // - What happens if we click on a task in the calendar and want to edit it, how does the UI behave?
 
   @tracked calendarEvents
